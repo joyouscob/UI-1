b10d7e56c6f41301461a6b3f4f6f0b1dcb4485f5
William Falk-Wallace <wfalkwallace@gmail.com>
2013-11-14 01:13:30 -0500

    adds play pause functionality to button

--- hw3/js/main.js
+++ hw3/js/main.js
@@ -1,4 +1,15 @@
-function play()
-{
-$("#video").play();
-}
+$(function () {
+
+
+	$('#pause').click(function() {
+		$('video').get(0).pause();
+	});
+	
+	$('#play').click(function() {
+		$('video').get(0).play();
+	});
+	
+
+
+
+});
