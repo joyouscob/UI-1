939e76ff127ec704c715b6434327f2abc576c852
William Falk-Wallace <wfalkwallace@gmail.com>
2013-11-14 15:31:21 -0500

    working recognizer functions

--- hw3/js/main.js
+++ hw3/js/main.js
@@ -1,58 +1,51 @@
 $(function () {
 
+	// $('#pause').click(function() {
+	// 	$('video').get(0).pause();
+	// });
 
-	$('#pause').click(function() {
-		$('video').get(0).pause();
-	});
-	
-	$('#play').click(function() {
-		$('video').get(0).play();
-	});
-	
-	
-	
-	
+	// $('#play').click(function() {
+	// 	$('video').get(0).play();
+	// });
+
+	var _isDown, _points, _strokeID, _r, _g, _rc; // global variables
+
+	_points = new Array(); // point array for current stroke
+	_strokeID = 0;
+	_r = new PDollarRecognizer();
+	console.dir(_r);
+
+	var canvas = document.getElementById('myCanvas');
+	console.dir(canvas);
+	_g = canvas.getContext('2d');
+	_g.lineWidth = 3;
+	_g.font = ".25em Gentilis";
+	_rc = getCanvasRect(canvas); // canvas rect on page
+	_g.fillStyle = "rgb(255,255,136)";
+	_g.fillRect(0, 0, _rc.width, 5);
+
+	_isDown = false;
 	
 	$("#myCanvas").mousedown(function(event) {
 		// console.dir(event);
 		mouseDownEvent(event.clientX, event.clientY, event.button);
 	});
-	
+
 	$("#myCanvas").mousemove(function(event) {
 		// console.dir(event);
 		// console.dir(event.clientX);
-		mouseDownEvent(event.clientX, event.clientY, event.button);
-	});
-	
-	$("#myCanvas").mouseup(function(event) {
-		// console.dir(event);
 		mouseMoveEvent(event.clientX, event.clientY, event.button);
 	});
-	
-	var _isDown, _points, _r, _g, _rc;
-	
-	_points = new Array();
-	_r = new PDollarRecognizer();
-	
-	var canvas = jQuery.getElementById('myCanvas');
-	_g = canvas.getContext('2d');
-	_g.fillStyle = "rgb(0,0,225)";
-	_g.strokeStyle = "rgb(0,0,225)";
-	_g.lineWidth = 3;
-	_g.font = "16px Gentilis";
-	_rc = getCanvasRect(canvas); // canvas rect on page
-	_g.fillStyle = "rgb(255,255,136)";
-	_g.fillRect(0, 0, _rc.width, 20);
 
-	_isDown = false;
-	
-	
-	
+	$("#myCanvas").mouseup(function(event) {
+		// console.dir(event);
+		mouseUpEvent(event.clientX, event.clientY, event.button);
+	});
 
-	
-	
+
 	function getCanvasRect(canvas)
 	{
+		console.dir(canvas);
 		var w = canvas.width;
 		var h = canvas.height;
 
@@ -71,110 +64,144 @@ $(function () {
 		var scrollY = 0;
 		if (typeof(document.body.parentElement) != 'undefined')
 		{
-				scrollY = document.body.parentElement.scrollTop; // IE
+					scrollY = document.body.parentElement.scrollTop; // IE
+				}
+				else if (typeof(window.pageYOffset) != 'undefined')
+				{
+					scrollY = window.pageYOffset; // FF
+				}
+				return scrollY;
 			}
-			else if (typeof(window.pageYOffset) != 'undefined')
+			//
+			// Mouse Events
+			//
+			function mouseDownEvent(x, y, button)
 			{
-				scrollY = window.pageYOffset; // FF
+				document.onselectstart = function() { return false; } // disable drag-select
+				document.onmousedown = function() { return false; } // disable drag-select
+				if (button <= 1)
+				{
+					_isDown = true;
+					x -= _rc.x;
+					y -= _rc.y - getScrollY();
+					if (_strokeID == 0) // starting a new gesture
+					{
+						_points.length = 0;
+						_g.clearRect(0, 0, _rc.width, _rc.height);
+					}
+					_points[_points.length] = new Point(x, y, ++_strokeID);
+					drawText("Recording stroke #" + _strokeID + "...");
+					var clr = "rgb(" + rand(0,200) + "," + rand(0,200) + "," + rand(0,200) + ")";
+					_g.strokeStyle = clr;
+					_g.fillStyle = clr;
+					_g.fillRect(x - 4, y - 3, 9, 9);
+				}
+				else if (button == 2)
+				{
+					drawText("Recognizing gesture...");
+				}
 			}
-			return scrollY;
-		}
-		//
-		// Mouse Events
-		//
-		function mouseDownEvent(x, y)
-		{
-			document.onselectstart = function() { return false; } // disable drag-select
-			document.onmousedown = function() { return false; } // disable drag-select
-			_isDown = true;
-			x -= _rc.x;
-			y -= _rc.y - getScrollY();
-			if (_points.length > 0)
-				_g.clearRect(0, 0, _rc.width, _rc.height);
-			_points.length = 1; // clear
-			_points[0] = new Point(x, y);
-			drawText("Recording unistroke...");
-			_g.fillRect(x - 4, y - 3, 9, 9);
-		}
-		function mouseMoveEvent(x, y)
-		{
-			if (_isDown)
+			function mouseMoveEvent(x, y, button)
 			{
-				x -= _rc.x;
-				y -= _rc.y - getScrollY();
-				_points[_points.length] = new Point(x, y); // append
-				drawConnectedPoint(_points.length - 2, _points.length - 1);
+				if (_isDown)
+				{
+					x -= _rc.x;
+					y -= _rc.y - getScrollY();
+					_points[_points.length] = new Point(x, y, _strokeID); // append
+					drawConnectedPoint(_points.length - 2, _points.length - 1);
+				}
 			}
-		}
-		function mouseUpEvent(x, y)
-		{
-			document.onselectstart = function() { return true; } // enable drag-select
-			document.onmousedown = function() { return true; } // enable drag-select
-			if (_isDown)
+			function mouseUpEvent(x, y, button)
 			{
-				_isDown = false;
+				document.onselectstart = function() { return true; } // enable drag-select
+				document.onmousedown = function() { return true; } // enable drag-select
+				if (button <= 1)
+				{
+					if (_isDown)
+					{
+						_isDown = false;
+						drawText("Stroke #" + _strokeID + " recorded.");
+					}
+				}
+				else if (button == 2) // segmentation with right-click
+				{
+					if (_points.length >= 10)
+					{
+						var result = _r.Recognize(_points);
+						drawText("Result: " + result.Name + " (" + round(result.Score,2) + ").");
+					}
+					else
+					{
+						drawText("Too little input made. Please try again.");
+					}
+					_strokeID = 0; // signal to begin new gesture on next mouse-down
+				}
+			}
+			function drawConnectedPoint(from, to)
+			{
+				_g.beginPath();
+				_g.moveTo(_points[from].X, _points[from].Y);
+				_g.lineTo(_points[to].X, _points[to].Y);
+				_g.closePath();
+				_g.stroke();
+			}
+			function drawText(str)
+			{
+				_g.fillStyle = "rgb(255,255,136)";
+				_g.fillRect(0, 0, _rc.width, 5);
+				_g.fillStyle = "rgb(0,0,255)";
+				_g.fillText(str, 1, 4);
+			}
+			function rand(low, high)
+			{
+				return Math.floor((high - low + 1) * Math.random()) + low;
+			}
+			function round(n, d) // round 'n' to 'd' decimals
+			{
+				d = Math.pow(10, d);
+				return Math.round(n * d) / d
+			}
+			//
+			// Multistroke Adding and Clearing
+			//
+			function onClickAddExisting()
+			{
 				if (_points.length >= 10)
 				{
-					var result = _r.Recognize(_points, document.getElementById('useProtractor').checked);
-					drawText("Result: " + result.Name + " (" + round(result.Score,2) + ").");
+					var pointclouds = document.getElementById('pointclouds');
+					var name = pointclouds[pointclouds.selectedIndex].value;
+					var num = _r.AddGesture(name, _points);
+					drawText("\"" + name + "\" added. Number of \"" + name + "\"s defined: " + num + ".");
+					_strokeID = 0; // signal to begin new gesture on next mouse-down
 				}
-				else // fewer than 10 points were inputted
+			}
+			function onClickAddCustom()
+			{
+				var name = document.getElementById('custom').value;
+				if (_points.length >= 10 && name.length > 0)
 				{
-					drawText("Too few points made. Please try again.");
+					var num = _r.AddGesture(name, _points);
+					drawText("\"" + name + "\" added. Number of \"" + name + "\"s defined: " + num + ".");
+					_strokeID = 0; // signal to begin new gesture on next mouse-down
 				}
 			}
-		}
-		function drawText(str)
-		{
-			_g.fillStyle = "rgb(255,255,136)";
-			_g.fillRect(0, 0, _rc.width, 20);
-			_g.fillStyle = "rgb(0,0,255)";
-			_g.fillText(str, 1, 14);
-		}
-		function drawConnectedPoint(from, to)
-		{
-			_g.beginPath();
-			_g.moveTo(_points[from].X, _points[from].Y);
-			_g.lineTo(_points[to].X, _points[to].Y);
-			_g.closePath();
-			_g.stroke();
-		}
-		function round(n, d) // round 'n' to 'd' decimals
-		{
-			d = Math.pow(10, d);
-			return Math.round(n * d) / d
-		}
-		//
-		// Unistroke Adding and Clearing
-		//
-		function onClickAddExisting()
-		{
-			if (_points.length >= 10)
+			function onClickCustom()
 			{
-				var unistrokes = document.getElementById('unistrokes');
-				var name = unistrokes[unistrokes.selectedIndex].value;
-				var num = _r.AddGesture(name, _points);
-				drawText("\"" + name + "\" added. Number of \"" + name + "\"s defined: " + num + ".");
+				document.getElementById('custom').select();
 			}
-		}
-		function onClickAddCustom()
-		{
-			var name = document.getElementById('custom').value;
-			if (_points.length >= 10 && name.length > 0)
+			function onClickDelete()
 			{
-				var num = _r.AddGesture(name, _points);
-				drawText("\"" + name + "\" added. Number of \"" + name + "\"s defined: " + num + ".");
+				var num = _r.DeleteUserGestures(); // deletes any user-defined templates
+				alert("All user-defined gestures have been deleted. Only the 1 predefined gesture remains for each of the " + num + " types.");
+				_strokeID = 0; // signal to begin new gesture on next mouse-down
 			}
-		}
-		function onClickCustom()
-		{
-			document.getElementById('custom').select();
-		}
-		function onClickDelete()
-		{
-			var num = _r.DeleteUserGestures(); // deletes any user-defined unistrokes
-			alert("All user-defined gestures have been deleted. Only the 1 predefined gesture remains for each of the " + num + " types.");
-		}
+			function onClickClearStrokes()
+			{
+				_points.length = 0;
+				_strokeID = 0;
+				_g.clearRect(0, 0, _rc.width, _rc.height);
+				drawText("Canvas cleared.");
+			}
 
 
 
@@ -188,6 +215,4 @@ $(function () {
 
 
 
-
-
-	});
+		});
